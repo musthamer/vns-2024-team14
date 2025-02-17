cac3198dd783cc314b7ecefdc891e58e80bca2ea
Mert <meroezdemir@smail.hs-bremerhaven.de>
2025-02-17 20:47:12 +0100

    docker-mariadb-v1

--- docker-full/docker-mariadb/context/myinit.sh
+++ docker-full/docker-mariadb/context/myinit.sh
@@ -7,21 +7,38 @@ trap onexit SIGTERM
 function onexit {
   #shutdown ...
   echo shutting down >> $log
-  /opt/tomcat/bin/shutdown.sh
+  /etc/init.d/mariadb stop
   exit
 }
 echo trap handler set >> $log 
 # start services
 /etc/init.d/mariadb start
-sudo -u mysql mariadb -e "drop database if exists dbdemo;
-drop user if exists dbuser@'localhost';
-drop user if exists dbuser@'%';
-create database dbdemo;
-create user dbuser@'%' identified by 'complexpassword';
-grant all privileges on dbdemo.* TO dbuser@'%';
-flush privileges;
-use dbdemo;
-create table demo (id int primary key auto_increment, name text);"
+
+#initialize database
+mysql -u root -e "
+
+CREATE DATABASE IF NOT EXISTS todo_app;
+CREATE USER IF NOT EXISTS dbuser@'%' IDENTIFIED BY 'vnsteam14';
+GRANT ALL PRIVILEGES ON todo_app.* TO dbuser@'%';
+FLUSH PRIVILEGES;
+
+USE todo_app
+
+CREATE TABLE IF NOT EXISTS users (
+id INT AUTO_INCREMENT PRIMARY KEY, 
+name VARCHAR(100) NOT NULL
+email VARCHAR(255) NOT NULL UNIQUE
+password VARCHAR (255) NOT NULL
+salt VARCHAR(255) NOT NULL);
+
+CREATE TABLE IF NOT EXISTS sessions (
+sessions_id CHAR(36) PRIMARY KEY,
+email VARCHAT(255) NOT NULL,
+created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+expires_at DATETIME NOT NULL,
+FOREIGN KEY (email) REFERENCES users(email) ON DELETE CASCADE
+);
+"
 
 while true; do
   echo "$(date +%FT%T) ping" >> $log
