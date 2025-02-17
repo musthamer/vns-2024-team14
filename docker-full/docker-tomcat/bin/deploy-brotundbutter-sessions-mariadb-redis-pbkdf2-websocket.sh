435249e005580f6106d7a6912a94c81d2d3c1c0a
celakkoeprue <celakkoeprue@smail.hs-bremerhaven.de>
2025-02-17 20:58:45 +0100

    tomcat

--- docker-full/docker-tomcat/bin/deploy-brotundbutter-sessions-mariadb-redis-pbkdf2-websocket.sh
+++
@@ -1,2 +0,0 @@
-war=context/brotundbutter-sessions-mariadb-redis-pbkdf2-websocket.war
-curl --location --netrc-file local/netrc --fail --upload-file "$war" "http://localhost:8080/manager/text/deploy?path=/app&update=true"
