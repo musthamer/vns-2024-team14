26ce7b8a44e2d75b1417770d0bcc47fa2d744d7f
mo alzubaidy <mohalzubaidy@informatik.hs-bremerhaven.de>
2025-03-05 02:24:51 +0100

    {}

--- docker-final/docker-haproxy/context/lasttest/wrk_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-duration="30s"
-threads=2
-connections=50
-url="http://haproxy:80"
-
-echo "Starte wrk Lasttest für $url mit $connections Verbindungen für $duration..."
-wrk -t$threads -c$connections -d$duration $url
